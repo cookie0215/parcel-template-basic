a4f4b6159c0e12640d4c683b0c7b1c829e8ad90e
cookie0215 <jina_0215@naver.com>
2021-12-28 21:18:13 +0900

    create parcel-templete-basic

---
+++ .postcssrc.js
@@ -0,0 +1,9 @@
+// import autoprefixer from 'autoprefixer'
+const autoprefixer = require('autoprefixer');
+
+// export {plugins: [autoprefixer]}
+module.exports = {
+  plugins: [
+    autoprefixer
+  ]
+}
